7739e30efb68fe1431450b35d134c29e410692bc
Dan Actor <danltactor@gmail.com>
2018-02-17 15:14:14 -0700

    basic styles, add styling npms

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { 
+  Header,
+  Form,
+  Input,
+  Image,
+  Button,
+  Card,
+  Container,
+} from 'semantic-ui-react';
 
 class Home extends Component {
+
+  addPlayer = () => {
+    console.log('do it')
+  }
+
+  displayPlayers = () => {
+    // TODO
+  }
+
   render() {
     return (
-      <Header as='h1' textAlign='center'>MTG Counter</Header>
+      <Container>
+        <Header style={styles.header} as='h1' textAlign='center'>
+          MTG Counter
+          <br />
+          <Button color='green' textAlign='center' onClick={this.addPlayer}>
+            <Header style={styles.header} as='h2' textAlign='center'>Add Player</Header>
+          </Button>
+        </Header>
+        <Card.Group>
+          { this.displayPlayers }
+        </Card.Group>
+      </Container>
     );
+  }
+}
+
+const styles = {
+  iframe: {
+    width: '100%',
+    height: '100vh'
+  },
+  centered: {
+    margin: '0 auto',
+  },
+  header: {
+    color: '#FFF'
   }
 }
 
